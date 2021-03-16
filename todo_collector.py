18b3f4e5822953b357f1a04baaa85520af29ed03
SimulacraAtTanagra <shaneayers1@gmail.com>
2021-03-16 14:39:57 -0400

    updated todo_collector.py

--- todo_collector.py
+++ todo_collector.py
@@ -6,6 +6,7 @@ Created on Tue Mar 16 14:07:28 2021
 """
 
 import os
+import sys
 
 def todo_collect(repofolder): #collects TODO items from files
     subdirs = [x[0] for x in os.walk(repofolder) if "src" not in x[0] and "git" not in x[0]]          
@@ -15,18 +16,21 @@ def todo_collect(repofolder): #collects TODO items from files
             filename=os.path.join(foldername,file)
             with open(filename,'r') as f:
                 xyz=f.readlines()
-            xyz=[f'{file} - line {ix} - {x.strip()}' for ix,x in enumerate(xyz) if "#TODO" in x]
+            xyz=[f'{file} - line {ix} - {x.strip()}' for ix,x in enumerate(xyz) if "#TODO" in x and '"#TODO"' not in x]
             final.extend(xyz)
     return(sorted(list(set(final))))
-def main(repofolder=None):
+        
+def main(repofolder=None,outfile=None):
     if repofolder:
         repofolder=repofolder
     else:
         repofolder='c://where//you//keep//repos'
     todos=todo_collect(repofolder)
-    for ix,todo in enumerate(todos):
-        print(ix+1,todo)
-        
+    if outfile:
+        print(todos,file=outfile)
+    else:
+        for ix,todo in enumerate(todos):
+            print(ix+1,todo)
     
 if __name__=="__main__":
     main(repofolder=REPO)
