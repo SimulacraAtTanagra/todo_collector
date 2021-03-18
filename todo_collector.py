eaab413b1a8fd7fcabb06f15fba9f2644cd1bf5c
SimulacraAtTanagra <shaneayers1@gmail.com>
2021-03-18 18:02:01 -0400

    updated todo_collector.py

--- todo_collector.py
+++ todo_collector.py
@@ -4,28 +4,36 @@ Created on Tue Mar 16 14:07:28 2021
 
 @author: shane
 """
-
+from typing import List
 import os
 import sys
 
-def todo_collect(repofolder): #collects TODO items from files
-    subdirs = [x[0] for x in os.walk(repofolder) if "src" not in x[0] and "git" not in x[0]]          
+def get_py_files(path):
+    #find all py files, even if there is nesting
+    filelist=[]
+    for dirpath, subdirs, files in os.walk(path):
+        filelist.extend([os.path.join(dirpath, x) for x in files if x.endswith('.py')])
+    return(filelist)
+
+def todo_collect(filenames:List[str])-> List[str]: 
+    """
+    Accepts list of filenames and returns sorted set list of TODOs
+    """
     final=[]
-    for foldername in subdirs:
-        for file in [y for y in os.listdir(foldername) if '.py' in y]:
-            filename=os.path.join(foldername,file)
-            with open(filename,'r') as f:
-                xyz=f.readlines()
-            xyz=[f'{file} - line {ix} - {x.strip()}' for ix,x in enumerate(xyz) if "#TODO" in x and '"#TODO"' not in x]
-            final.extend(xyz)
+    for filename in filenames:
+        with open(filename,'r') as f:
+            xyz=f.readlines()
+        file=filename.split('\\')[-1]
+        xyz=[f'{file} - line {ix} - {x.strip()}' for ix,x in enumerate(xyz) if "#TODO" in x and '"#TODO"' not in x]
+        final.extend(xyz)
     return(sorted(list(set(final))))
         
 def main(repofolder=None,outfile=None):
     if repofolder:
         repofolder=repofolder
     else:
-        repofolder='c://where//you//keep//repos'
-    todos=todo_collect(repofolder)
+        repofolder=os.getcwd()
+    todos=todo_collect(get_py_files(repofolder))
     if outfile:
         print(todos,file=outfile)
     else:
@@ -33,4 +41,4 @@ def main(repofolder=None,outfile=None):
             print(ix+1,todo)
     
 if __name__=="__main__":
-    main(repofolder=REPO)
+    main()
